8242942b9c2fd3929e866e7ed72f4942447c83b9
unknown <jacobmarcosoliver@gmail.com>
2023-09-29 04:09:22 +0330

    empty host error catched but code still is monkey patch

--- server.py
+++ server.py
@@ -11,7 +11,9 @@ while True:
     t = cmd("adb connect localhost:5555")
     devices = cmd("adb devices")
     print(t)
-    if b'already connected' in t and b'offline' not in devices:
-        x = subprocess.check_output(
-            ['adb', '-s', 'localhost:5555', 'forward', 'tcp:4200', 'tcp:4200'])
-        print(x)
+    if b'already connected' in t and b'offline' not in devices and b'error: device' not in t and b'unable' not in t:
+        forwardResult = cmd("adb -s localhost:5555 forward tcp:4200 tcp:4200")
+        print(forwardResult)
+    if b'empty host name' in t:
+        cmd("adb kill-server")
+        cmd("adb start-server")
