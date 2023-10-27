d9a1917dad24bb0f16a7003bfb2395832cc6788a
unknown <jacobmarcosoliver@gmail.com>
2023-10-27 10:24:44 +0330

    fixed bug loop could not forward traffic

--- server.py
+++ server.py
@@ -11,53 +11,61 @@ while True:
         print("=========================================")
         print(
             "-> trying to connect to localhost:5555")
-        t = cmd("adb connect localhost:5555")
+        resultConnectLocalhost = cmd("adb connect localhost:5555")
+        if b'unable' in resultConnectLocalhost:
+            sleep(1)
+            continue
+        print(
+            "[+] connected to localhost:5555")
+        itsFirstTimeConnect = True
+        while True:
+            try:
+                devices = cmd("adb devices")
+                if b'localhost:5555' in devices:
+                    if itsFirstTimeConnect:
+                        while True:
+                            try:
+                                print(
+                                    "-> trying to forward traffic from windows to bluestack with port 63254 for HTTP")
+                                forwardResult = cmd(
+                                    "adb -s localhost:5555 forward tcp:63254 tcp:63254")
+                                print(
+                                    "[+] traffic forwarded with port 63254 from windows to bluestack")
+                                print(
+                                    "-> trying to forward traffic from windows to bluestack with port 63255 for SOCKS")
+                                forwardResult = cmd(
+                                    "adb -s localhost:5555 forward tcp:63255 tcp:63255")
+                                print(
+                                    "[+] traffic forwarded with port 63255 from windows to bluestack")
+                                itsFirstTimeConnect = False
+                                print("[+] status: all ready done.")
+                                break
+                            except subprocess.CalledProcessError as e:
+                                print("[-] could not forward traffic!")
+                                sleep(1)
+                                break
+
+                    else:
+                        sleep(1)
+                else:
+                    sleep(1)
+                    break
+            except subprocess.CalledProcessError as e:
+                sleep(1)
+                continue
     except subprocess.CalledProcessError as e:
         try:
             print(
                 "-> trying to: adb kill-server")
             cmd("adb kill-server")
         except subprocess.CalledProcessError as e:
-            print("[-] could kill the server")
+            print("[-] could not kill the server")
             continue
         try:
             print(
                 "-> trying to: adb start-server")
             cmd("adb start-server")
-        except subprocess.CalledProcessError as e:
-            print("[-] could kill the server")
             continue
-    itsFirstTimeConnect = True
-    while True:
-        try:
-            devices = cmd("adb devices")
-            if b'localhost:5555' in devices:
-                if itsFirstTimeConnect:
-                    print("[+] connected to localhost:5555 Bluestack")
-                    while True:
-                        try:
-                            print(
-                                "-> trying to forward traffic from windows to bluestack with port 63254 for HTTP")
-                            print(
-                                "-> trying to forward traffic from windows to bluestack with port 63255 for SOCKS")
-                            forwardResult = cmd(
-                                "adb -s localhost:5555 forward tcp:63254 tcp:63254")
-                            forwardResult = cmd(
-                                "adb -s localhost:5555 forward tcp:63255 tcp:63255")
-                            print(
-                                "[+] traffic forwarded with port 4200 from windows to bluestack")
-                            itsFirstTimeConnect = False
-                            print("[+] status: all ready done.")
-                            break
-                        except subprocess.CalledProcessError as e:
-                            print("[-] could not forward traffic!")
-
-                        sleep(1)
-                else:
-                    sleep(1)
-            else:
-                sleep(1)
-                break
         except subprocess.CalledProcessError as e:
-            sleep(1)
+            print("[-] could not start the server")
             continue
